f03a331294395321a61dc7013c77a7ad2eb3f684
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-10-25 16:38:01 -0400

    Midterm: Config database

--- config/db.js
+++ config/db.js
@@ -1,18 +1,25 @@
+/*
+File name: config/db.js
+Student name: Cong Lanh Hoang
+Student ID: 301210743
+Date: October 25, 2022
+*/
+
 // Do not expose your credentials in your code.
-let atlasDB = "mongodb+srv://<username>:<passoword>@<cluster>/todo?retryWrites=true&w=majority";
+let atlasDB =
+  "mongodb+srv://dbuser:PZO6wjxARk92rkCr@cluster018.so2vhuv.mongodb.net/todo?retryWrites=true&w=majority";
 
 // Database setup
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
-module.exports = function(){
+module.exports = function () {
+  mongoose.connect(atlasDB);
+  let mongodb = mongoose.connection;
 
-    mongoose.connect(atlasDB);
-    let mongodb = mongoose.connection;
+  mongodb.on("error", console.error.bind(console, "Connection Error:"));
+  mongodb.once("open", () => {
+    console.log("===> Connected to MongoDB.");
+  });
 
-    mongodb.on('error', console.error.bind(console, 'Connection Error:'));
-    mongodb.once('open', ()=>{
-        console.log('===> Connected to MongoDB.');
-    })
-
-    return mongodb;
-}
+  return mongodb;
+};
