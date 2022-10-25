4462931a8c7a27e79cc5aad2b4c274f80de5718f
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-10-25 18:07:45 -0400

    Midterm: Protect Add, Edit, Delete routes

--- routes/todo.router.js
+++ routes/todo.router.js
@@ -28,16 +28,16 @@ router.get("/list", todoController.todoList);
 router.get("/details/:id", todoController.details);
 
 // Routers for edit
-router.get("/edit/:id", todoController.displayEditPage);
-router.post("/edit/:id", todoController.processEditPage);
+router.get("/edit/:id", requireAuth, todoController.displayEditPage);
+router.post("/edit/:id", requireAuth, todoController.processEditPage);
 
 // Delete
-router.get("/delete/:id", todoController.performDelete);
+router.get("/delete/:id", requireAuth, todoController.performDelete);
 
 /* GET Route for displaying the Add page - CREATE Operation */
-router.get("/add", todoController.displayAddPage);
+router.get("/add", requireAuth, todoController.displayAddPage);
 
 /* POST Route for processing the Add page - CREATE Operation */
-router.post("/add", todoController.processAddPage);
+router.post("/add", requireAuth, todoController.processAddPage);
 
 module.exports = router;
