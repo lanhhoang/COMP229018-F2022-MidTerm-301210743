27d1505286862051f1899b61b8536e5adfe25479
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-10-26 23:56:59 -0400

    Midterm: Import getErrorMessage to UserController

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,28 +7,7 @@ Date: October 25, 2022
 
 let User = require("../models/user.model");
 let passport = require("passport");
-
-function getErrorMessage(err) {
-  console.log("===> Erro: " + err);
-  let message = "";
-
-  if (err.code) {
-    switch (err.code) {
-      case 11000:
-      case 11001:
-        message = "Username already exists";
-        break;
-      default:
-        message = "Something went wrong";
-    }
-  } else {
-    for (var errName in err.errors) {
-      if (err.errors[errName].message) message = err.errors[errName].message;
-    }
-  }
-
-  return message;
-}
+const getErrorMessage = require("../helpers/getErrorMessage");
 
 module.exports.renderSignin = function (req, res, next) {
   if (!req.user) {
